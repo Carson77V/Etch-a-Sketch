bc9a1636ace03f70c89ee64eb8e8a2189ba490f3
Carson <carsontwj@gmail.com>
2022-11-09 21:04:24 -0500

    Add loop to createGrid() to generate 16x16

--- javascript.js
+++ javascript.js
@@ -2,14 +2,16 @@
 function createGrid(){
     // select the div where the grib will be placed
     const grid = document.querySelector('.grid');
-
+    for (let i = 0; i < 16; i++){
     // this creates one block in the grid
     const gridBlock = document.createElement('div');
     gridBlock.classList.add('gridBlock');
     gridBlock.setAttribute('style', 'border-style: solid; border-color: black');
 
     // appends gridBlock to the grid div
+    
     grid.appendChild(gridBlock);
+    }
 }
 
 createGrid();
