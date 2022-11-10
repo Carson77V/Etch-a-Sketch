9c6cb37d81b4038425e81b0a86773f56f43c6ec8
Carson <carsontwj@gmail.com>
2022-11-09 21:29:10 -0500

    Style 16X16 grid
    Make grid generate 4 boxes per row, add border and use flexbox

--- javascript.js
+++ javascript.js
@@ -2,15 +2,15 @@
 function createGrid(){
     // select the div where the grib will be placed
     const grid = document.querySelector('.grid');
-    for (let i = 0; i < 16; i++){
-    // this creates one block in the grid
-    const gridBlock = document.createElement('div');
-    gridBlock.classList.add('gridBlock');
-    gridBlock.setAttribute('style', 'border-style: solid; border-color: black');
 
-    // appends gridBlock to the grid div
-    
-    grid.appendChild(gridBlock);
+    // the loop generates a 16x16 grid
+    for (let i = 0; i < 16; i++){
+        // this creates one block in the grid
+        const gridBlock = document.createElement('div');
+        gridBlock.classList.add('gridBlock');
+
+        // appends gridBlock to the grid div
+        grid.appendChild(gridBlock);
     }
 }
 
